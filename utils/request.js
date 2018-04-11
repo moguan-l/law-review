3af242fd3b27718eb278d7caa5bb7cd41339fb21
LQJ <1530212104@qq.com>
2018-04-11 22:01:15 +0800

    违法拍、完善资料

--- utils/request.js
+++ utils/request.js
@@ -3,6 +3,10 @@ export default ({url, method, data}) => {
         wx.request({
             url, method, data,
             success: res => {
+                let {errcode} = res.data;
+                if (errcode  === 602 || errcode === 610) {
+                    return wx.reLaunch({url: '/pages/login/index'})
+                }
                 success(res.data)
             },
             fail
